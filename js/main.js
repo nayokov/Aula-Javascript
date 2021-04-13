e8d58d478cda2c54e38b3ca90823464f831198fb
Nayan Guimarães Ribeiro <nayandf@gmail.com>
2021-04-13 09:53:34 -0300

    ok

---
+++ js/main.js
@@ -0,0 +1,20 @@
+/*
+var nome ="Nayan Guimarães Ribeiro";
+var idade = 29;
+var dict =[{nome :"maça", cor:"vermelha"},{nome :"caqui", cor:"amarelo"}]
+var lista= ["banana","Maçã","Goiaba"];
+var lista2= ["banana","Maçã","Goiaba"];
+alert("Bem vindo "+ nome +" Idade : "+ idade);
+alert(lista2);
+
+console.log(dict);
+console.log(lista);
+*/
+var data = new Date();
+var idade = prompt("Qual sua idade?");
+if(idade>=18){
+    alert("Maior de idade");
+}else{
+    alert("Menor de idade")
+}
+alert(data);
